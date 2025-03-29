6c2374542a7f347d8f0557a183e50e4096ebaf31
Ksonson <Kenny941219@gmail.com>
2025-03-30 01:42:30 +0800

    getline

---
+++ input/getline_char.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{   
+    cout << "Enter a something: ";
+    char ch[20];
+    cin.getline(ch,10);
+    cout << ch << endl;
+
+    return 0;
+}
---
+++ input/getline_string.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{   
+    cout << "Enter a string: ";
+    string str;
+    getline(cin, str);
+    cout << str << endl;
+    return 0;
+}
