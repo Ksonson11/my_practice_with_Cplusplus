2ce69bd525d25b14570dace18e38368d2e8fb732
Ksonson <Kenny941219@gmail.com>
2025-04-21 01:06:34 +0800

    new folder

---
+++ header/iomanip/setw_setfill.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{   
+    cout << setw(10) << setfill('*') << "" << endl;
+    cout << setw(10) << 2025 << endl;
+    cout << setw(10)<< setfill("") << 50;
+    
+
+    return 0;
+}
